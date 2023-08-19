307a9730de4c1ea8df24e28ca869f1b91aab8bef
rirubelhossain <rubelphp94@gmail.com>
2023-08-19 18:47:14 +0600

    work with deposit section

--- js/deposit.js
+++ js/deposit.js
@@ -1,1 +1,13 @@
 // console.log("deposit")
+
+// step-1 : add event lister with deposit button
+document.getElementById("deposit_button").addEventListener("click", function(){
+    const depositFlied = document.getElementById("deposit_input") ;
+    const depositAmount = depositFlied.value ; // get the deposit input field
+
+    // document.getElementById("deposit_out").innerText = depositAmount
+    const depositTotalElement = document.getElementById("deposit_out") ;
+    depositTotalElement.innerText = depositAmount ;
+    
+    depositFlied.value = "" ;
+})
